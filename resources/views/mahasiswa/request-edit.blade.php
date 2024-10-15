5a07c3e29885cfae148241642d8f8669b4dd60e4
Muhammad Sidiq H. <muhammadsidiq.intern@gmail.com>
2024-10-15 08:43:17 +0700

    update: fix error validation

--- resources/views/mahasiswa/request-edit.blade.php
+++ resources/views/mahasiswa/request-edit.blade.php
@@ -35,20 +35,19 @@
                         </div>
                         <x-forms.mahasiswa :action="route('mahasiswa.update-data-request', $mhs->id)" :mahasiswa="$mhs" />
                     </div>
+                @elseif ($request)
+                    <div
+                        class="flex justify-center items-center bg-blue-600 text-center font-medium my-5 text-white p-4 rounded-lg">
+                        <x-ri-timer-line class="w-8 h-8 sm:w-6 sm:h-6 mr-4  text-white" />
+                        <span>
+                            Permintaan anda sedang diproses, mohon untuk bersabar!
+                        </span>
+                    </div>
                 @else
-                    @if ($request !== null)
-                        <div class="flex justify-center items-center bg-blue-600 text-center font-medium my-5 text-white p-4 rounded-lg">
-                            <x-ri-timer-line class="w-8 h-8 sm:w-6 sm:h-6 mr-4  text-white" />
-                            <span>
-                                Permintaan anda sedang diproses, mohon untuk bersabar!
-                            </span>
-                        </div>
-                    @else
-                        <div class="mx-auto lg:mx-0 w-full lg:w-1/2 justi mt-6 bg-white shadow-md rounded-lg p-6">
-                            <h2 class="text-lg font-semibold text-gray-800 mb-4">{{ $mhs->name }}</h2>
-                            <x-forms.mahasiswa-card-request :action="route('mahasiswa.store-edit-request')" :mahasiswa="$mhs" />
-                        </div>
-                    @endif
+                    <div class="mx-auto lg:mx-0 w-full lg:w-1/2 justi mt-6 bg-white shadow-md rounded-lg p-6">
+                        <h2 class="text-lg font-semibold text-gray-800 mb-4">{{ $mhs->name }}</h2>
+                        <x-forms.mahasiswa-card-request :action="route('mahasiswa.store-edit-request')" :mahasiswa="$mhs" />
+                    </div>
                 @endif
             @endforeach
         @endif
